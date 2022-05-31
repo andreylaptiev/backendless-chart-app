f8d5228833208a41d62a8b1a6a1b2108062879f1
Andrey Laptiev <andreylaptiev13@gmail.com>
2022-05-31 18:18:30 +0300

    Add ChartApp and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import ChartApp from './apps/ChartApp';
 import './App.css';
 
 const App = () => {
-  return;
+  return (
+    <div className='app'>
+      <ChartApp />
+    </div>
+  );
 }
 
 export default App;
